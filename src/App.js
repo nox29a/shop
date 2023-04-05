98644cb350bca6864bfd0e7fa389259d0b40a30b
nox29a <adamczyk29a@interia.pl>
2023-04-05 15:36:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="text-2xl underline">
+    Hello world!
+  </h1>
       </header>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </div>
   );
 }
